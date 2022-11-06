c2c435b0eb2afb976d73e0956f6617a3ab9699d4
OfirOvadia96 <ofirrr999@gmail.com>
2022-11-07 00:05:28 +0200

    finish isParetoImprovement, almost finish with isParetoOptimal

--- task 1/task 1.py
+++ task 1/task 1.py
@@ -10,20 +10,33 @@ class Agent():
     OptionVal = {} # option-int : value-float
    
 
-def isParetoImprovement(agents:List[],option1:int,option2:int)->bool:
+
+def isParetoImprovement(agents:list, option1:int, option2:int)->bool:
+    '''
+    Description:
+    A function that checks whether option 1 is a Pareto improvement of option 2
+    Param :
+    List of value agents
+    option1(int) - An index that points to the attributed value of this option according to that agent
+    option2(int) - An index that points to the attributed value of this option according to that agent
+    Return:
+    True - If option 1 is a detail improvement of option 2
+    If there is at least one large value and all other values are equal or greater in option 1 than option 2
+    Otherwise false
+    '''
     #Denotes one or more agents option 1 gives it a higher value than option 2
     #True - Option 1 holds a higher value, False - Option 1 doesn't holds a higher value then 2
     flag = false
-    for(agent in agents):
-        if(agent.OptionVal[option1] < agent.OptionVal[option2]):
+    for agent in agents :
+        if agent.OptionVal[option1] < agent.OptionVal[option2]:
             return False
 
-        if(agent.OptionVal[option1] > agent.OptionVal[option2])
+        if agent.OptionVal[option1] > agent.OptionVal[option2]:
             flag = True
 
         #elif : equals then nothing..
 
-    if(flag):
+    if flag:
         return True #It is enough that one value is higher and all the others should be equal or higher
     
     return False # Because option1 and option2 have the same values in all agents
@@ -31,6 +44,15 @@ def isParetoImprovement(agents:List[],option1:int,option2:int)->bool:
 
 
 
-
-def isParetoOptimal(agents:List[Agent], option:int, allOptions:List[int])->bool:
+def isParetoOptimal(agents:list, option:int, allOptions:list)->bool:
+    '''
+    Description:
+    A function that checks whether an option is Pareto efficient
     
+    '''
+
+    for LoopOption in allOptions:
+        if isParetoImprovement(agents,LoopOption,option):
+            return False
+    
+    return True

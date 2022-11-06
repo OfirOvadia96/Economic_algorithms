ba7316b95a7ac42b699d23110ba610f12cb7219d
OfirOvadia96 <ofirrr999@gmail.com>
2022-11-07 00:18:20 +0200

    start tests

---
+++ task 1/TestPareto.py
@@ -0,0 +1,14 @@
+import unittest
+import Pareto
+
+class TestPareto(unittest.TestCase):
+
+
+    def test1(self):
+        self.assertEqual(0, 0)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()

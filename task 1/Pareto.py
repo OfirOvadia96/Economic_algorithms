6c575c59c9c96b42151c35c0a0781c7c264f15e2
OfirOvadia96 <ofirrr999@gmail.com>
2022-11-07 21:55:18 +0200

    finish tests on main in Pareto file

--- task 1/Pareto.py
+++ task 1/Pareto.py
@@ -10,15 +10,15 @@ class Agent():
     _OptionVal = {} # option-int : value-float
     
     def __init__(self):
-      self.OptionVal = dict()
+      self._OptionVal = dict()
 
     # getter method
     def value(self, option:int) -> float:
-        return self.OptionVal[option]
+        return self._OptionVal[option]
       
     # setter method
     def setOption(self, option:int, value:float):
-        self.OptionVal[option] = value
+        self._OptionVal[option] = value
    
 
 
@@ -37,7 +37,7 @@ def isParetoImprovement(agents:list, option1:int, option2:int)->bool:
     '''
     #Denotes one or more agents option 1 gives it a higher value than option 2
     #True - Option 1 holds a higher value, False - Option 1 doesn't holds a higher value then 2
-    flag = false
+    flag = False
     for agent in agents :
         if agent.value(option1) < agent.value(option2):
             return False
@@ -71,3 +71,100 @@ def isParetoOptimal(agents:list, option:int, allOptions:list)->bool:
             return False
     
     return True
+
+def isParetoImprovementTests():
+    Ami = Agent()
+    Tami = Agent()
+    Rami = Agent()
+    Ami.setOption(1,3)
+    Ami.setOption(2,2)
+    Tami.setOption(1,1)
+    Tami.setOption(2,1)
+    Rami.setOption(1,5)
+    Rami.setOption(2,5)
+    agents = [Ami,Tami,Rami]
+
+    #Check isParetoImprovement function when option1 is ParetoImprovement of option2
+    answerIsParetoImprovement = isParetoImprovement(agents,1,2)
+    if answerIsParetoImprovement:
+        print("pass answerIsParetoImprovement test")
+    else:
+        print("Falid answerIsParetoImprovement test")
+
+    #Check isParetoImprovement function for Equals values options
+    answerForEqualsOptions = isParetoImprovement(agents,1,1)
+    if answerForEqualsOptions is False:
+        print("pass answerForEqualsOptions test")
+    else:
+        print("Falid answerForEqualsOptions test")
+
+    #Check isParetoImprovement function when option1 is not ParetoImprovement of option2
+    Tami.setOption(1,0)
+    answerIsNotParetoImprovement = isParetoImprovement(agents,1,2)
+    if answerIsNotParetoImprovement is False:
+        print("pass answerIsNotParetoImprovement test")
+    else:
+        print("Falid answerIsNotParetoImprovement test")
+
+def isParetoOptimalTests():
+    Ami = Agent()
+    Tami = Agent()
+    Rami = Agent()
+    Ami.setOption(1,1)
+    Ami.setOption(2,2)
+    Ami.setOption(3,3)
+    Ami.setOption(4,4)
+    Ami.setOption(5,5)
+    Tami.setOption(1,3)
+    Tami.setOption(2,1)
+    Tami.setOption(3,2)
+    Tami.setOption(4,5)
+    Tami.setOption(5,4)
+    Rami.setOption(1,3)
+    Rami.setOption(2,5)
+    Rami.setOption(3,5)
+    Rami.setOption(4,1)
+    Rami.setOption(5,1)
+    agents = [Ami,Tami,Rami]
+    allOptions = [1,2,3,4,5]
+    #check option1
+    answerForOption1 = isParetoOptimal(agents,1,allOptions)
+    if answerForOption1:
+        print("pass answerForOption1 test")
+    else:
+        print("Falid answerForOption1 test")
+
+    #check option2
+    answerForOption2 = isParetoOptimal(agents,2,allOptions)
+    if answerForOption2 is False:
+        print("pass answerForOption2 test")
+    else:
+        print("Falid answerForOption2 test")
+
+    #check option3
+    answerForOption3 = isParetoOptimal(agents,3,allOptions)
+    if answerForOption3:
+        print("pass answerForOption3 test")
+    else:
+        print("Falid answerForOption3 test")
+
+    #check option4
+    answerForOption4 = isParetoOptimal(agents,4,allOptions)
+    if answerForOption4:
+        print("pass answerForOption4 test")
+    else:
+        print("Falid answerForOption4 test")
+
+    #check option5
+    answerForOption5 = isParetoOptimal(agents,5,allOptions)
+    if answerForOption5:
+        print("pass answerForOption5 test")
+    else:
+        print("Falid answerForOption5 test")
+
+
+if __name__ == '__main__':
+    isParetoImprovementTests()
+    isParetoOptimalTests()
+
+
--- task 1/TestPareto.py
+++ task 1/TestPareto.py
@@ -1,29 +1,56 @@
 import unittest
 from Pareto import Agent
+from Pareto import isParetoImprovement
+from Pareto import isParetoOptimal
 
 class TestPareto(unittest.TestCase):
 
     
     def isParetoImprovementTest(self):
-        setUpisParetoImprovementTest()
-        self.assertEqual(0, 0)
+        # agents = setUpisParetoImprovementTest()
+        # answer = isParetoImprovement(agents,1,2)
+        Ami = Agent()
+        Tami = Agent()
+        Rami = Agent()
+        Ami.setOption(1,3)
+        Ami.setOption(2,2)
+        Tami.setOption(1,1)
+        Tami.setOption(2,1)
+        Rami.setOption(1,5)
+        Rami.setOption(2,5)
+        agents = [Ami,Tami,Rami]
+        answer = isParetoImprovement(agents,1,2)
+        if(answer):
+            print("good")
+        self.assertTrue(answer)
+
+        
+
     
     
-    def isParetoOptimalTest(self):
-        setUpisParetoOptimalTest()
-        self.assertEqual(0, 0)
+    # def isParetoOptimalTest(self):
+    #     setUpisParetoOptimalTest()
+    #     self.assertEqual(0, 0)
 
 
-def setUpisParetoImprovementTest():
-    Ami = Agent()
-    Tami = Agent()
-    agents = [Ami,Tami]
+# def setUpisParetoImprovementTest() -> list:
+#     Ami = Agent()
+#     Tami = Agent()
+#     Rami = Agent()
+#     Ami.setOption(1,3)
+#     Ami.setOption(2,2)
+#     Tami.setOption(1,1)
+#     Tami.setOption(2,1)
+#     Rami.setOption(1,5)
+#     Rami.setOption(2,5)
+#     agents = [Ami,Tami,Rami]
+#     return agents
 
-def setUpisParetoOptimalTest():
-    Ami = Agent()
-    Tami = Agent()
-    Rami = Agent()
-    agents = [Ami,Tami,Rami]
+# def setUpisParetoOptimalTest():
+#     Ami = Agent()
+#     Tami = Agent()
+#     Rami = Agent()
+#     agents = [Ami,Tami,Rami]
 
 if __name__ == '__main__':
     unittest.main()
